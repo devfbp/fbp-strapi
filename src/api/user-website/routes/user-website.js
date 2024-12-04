f397476bd8756c0dbd41338431004aff7f21fdaf
dev <devs@fullbasketproperty.com>
2024-12-04 13:56:31 +0000

    user property

---
+++ src/api/user-website/routes/user-website.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-website router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-website.user-website');
